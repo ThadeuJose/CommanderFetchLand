a46de34650711e260b75a7fd431d2fe14f2ee430
Thadeu José <thadeujs@gmail.com>
2023-11-09 11:15:13 -0300

    Refactor

--- test/two-color.spec.ts
+++ test/two-color.spec.ts
@@ -19,13 +19,13 @@ import PathwayLandProcessor from "../app/js/lands-code/PathwayLandProcessor";
 
 describe("2 Color", function () {
   it("7 Fetch Lands", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: FetchLandProcessor = new FetchLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Arid Mesa"), 1);
     assert.strictEqual(actual.getAmount("Bloodstained Mire"), 1);
     assert.strictEqual(actual.getAmount("Flooded Strand"), 1);
@@ -36,148 +36,148 @@ describe("2 Color", function () {
   });
 
   it("1 Prismatic Vista", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: FetchLandProcessor = new FetchLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Prismatic Vista"), 1);
   });
 
   it("10 Basic Lands", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: BasicLandProcessor = new BasicLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Island"), 5);
     assert.strictEqual(actual.getAmount("Mountain"), 5);
   });
 
   it("1 Check Land", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: CheckLandProcessor = new CheckLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Sulfur Falls"), 1);
   });
 
   it("1 Crowd Land", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: CrowdLandProcessor = new CrowdLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Training Center"), 1);
   });
 
   it("1 Filter Land", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: FilterLandProcessor = new FilterLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Cascade Bluffs"), 1);
   });
   it("1 Shock Land", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: ShockLandProcessor = new ShockLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Steam Vents"), 1);
   });
   it("1 Pain Land", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: PainLandProcessor = new PainLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Shivan Reef"), 1);
   });
   it("1 Slow Land", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: SlowLandProcessor = new SlowLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Stormcarved Coast"), 1);
   });
   it("1 Horizon Land", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: HorizonLandProcessor = new HorizonLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Fiery Islet"), 1);
   });
   it("1 Command Tower", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: RainbowLandProcessor = new RainbowLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Command Tower"), 1);
   });
   it("1 Mana Confluence", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: RainbowLandProcessor = new RainbowLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Mana Confluence"), 1);
   });
   it("1 Dual Land", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: DualLandProcessor = new DualLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Volcanic Island"), 1);
   });
   it("1 Bounce Land", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: BounceLandProcessor = new BounceLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.getAmount("Izzet Boilerworks"), 1);
   });
   it("1 Pathway Land", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: PathwayLandProcessor = new PathwayLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(
       actual.getAmount("Riverglide Pathway / Lavaglide Pathway"),
       1
@@ -185,20 +185,20 @@ describe("2 Color", function () {
   });
 
   it("8 Utility Land", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     let processor: UtilityLandProcessor = new UtilityLandProcessor();
 
-    const actual = processor.process(colorManagerTest);
+    const actual = processor.process(colors);
     assert.strictEqual(actual.size(), 8);
   });
 
   it("38 Lands Total", function () {
-    let colorManagerTest: UserColorSelection = new UserColorSelection();
-    colorManagerTest.add(Color.Red);
-    colorManagerTest.add(Color.Blue);
+    let colors: UserColorSelection = new UserColorSelection();
+    colors.add(Color.Red);
+    colors.add(Color.Blue);
 
     const ProcessorArray: Processor[] = [
       new BasicLandProcessor(),
@@ -218,26 +218,9 @@ describe("2 Color", function () {
     ];
 
     const amount: number = ProcessorArray.reduce((sum, current) => {
-      return sum + current.process(colorManagerTest).size();
+      return sum + current.process(colors).size();
     }, 0);
 
     assert.strictEqual(amount, 38);
   });
 });
-
-// Fetch lands 	8 X
-// Prismatic Vista	1 X
-// Command Tower	1 X
-// Mana Confluence 1 X
-// Utility	8
-// Basics	10 X
-// Dual 	1 X
-// Shock 	1 X
-// crowd 	1 X
-// slow	1 X
-// pain	1 X
-// check	1 X
-// filter	1 X
-// horizon 	1 or City of Brass or Mount Doom X
-// bounce	1 X
-// 	38 make a size test
